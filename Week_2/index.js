9685d51290d72c5d225d140e88daee946441af0d
linaluonto <107812991+linaluonto@users.noreply.github.com>
2022-10-12 22:37:39 +0100

    Upload week 2

---
+++ Week_2/index.js
@@ -0,0 +1,95 @@
+// Level 1
+
+// 1
+let challenge = '30 Days Of JavaScript';
+
+// 2
+console.log(challenge)
+
+// 3
+console.log(challenge.length)
+
+// 4
+challenge.toUpperCase()
+
+// 5
+console.log(challenge.toLowerCase())
+
+// 6
+console.log(challenge.substring(0, 2))
+
+// 7
+console.log(challenge.substring(3))
+
+// 8
+console.log(challenge.includes('Script'))
+
+// 9
+console.log(challenge.split())
+
+// 10
+console.log(challenge.split(' '))
+
+// 11
+let list = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+console.log(list.split(','))
+
+// 12
+console.log(challenge.replace('JavaScript', 'Python'))
+
+// 13
+console.log(challenge.charAt(15))
+
+// 14
+console.log(challenge.charCodeAt(11))
+
+// 15
+console.log(challenge.indexOf('a'))
+
+// 16
+console.log(challenge.lastIndexOf('a'))
+
+// 17
+let sentence = 'You cannot end a sentence with because because because is a conjunction'
+console.log(sentence.indexOf('because'))
+
+// 18
+console.log(sentence.lastIndexOf('because'))
+
+// 19
+console.log(sentence.search('because'))
+
+// 20
+let trimWord = '\t trim me \t'
+console.log(trimWord.trim())
+
+// 21
+console.log(challenge.startsWith(3))
+
+// 22
+console.log(challenge.endsWith('Script'))
+
+// 23
+let findA = /a/gi
+console.log(challenge.match(findA))
+
+// 24
+let phrase = '30 Days Of'
+console.log(phrase.concat(' JavaScript'))
+
+// 25
+console.log(challenge.repeat(2))
+
+
+// Level 2
+
+// 1
+console.log('The quote \'There is no exercise better for the heart than\
+ reaching down and lifting people up.\' by John Holmes teaches us to help one another.')
+
+// 2
+console.log('\"Love is not patronizing and charity isn\'t about pity, it is about love.\
+ Charity and love are the same -- with charity you give love, so don\'t just \
+ give money but reach out your hand instead.\"')
+
+//  3
